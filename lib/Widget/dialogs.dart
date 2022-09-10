3634b710f8c7088b36a08aa79c183a10b1d166c4
nghkh <khoa.bach01@gmail.com>
2022-09-10 14:15:11 +0700

    updated dialog Widget

--- lib/Widget/dialogs.dart
+++ lib/Widget/dialogs.dart
@@ -1,21 +1,50 @@
+import 'package:baitap/Widget/button.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class AppDialog extends StatelessWidget {
   final Widget? icon;
   final String headingText;
+  final String contentText;
+  final Widget action;
 
-  const AppDialog({Key? key, required this.headingText, this.icon}) : super(key: key);
+  const AppDialog({
+    Key? key,
+    required this.headingText,
+    this.icon,
+    required this.contentText,
+    required this.action,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return AlertDialog(
-      icon: icon,
-      title: Text(
-        headingText,
-        style: TextStyle(
-          fontSize: 16,
-          color: Colors.black,
+    return Dialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30.0),
+      ),
+      child: Container(
+        child: Column(
+          children: [
+            Container(
+              child: icon,
+            ),
+            Text(
+              headingText,
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            Text(
+              contentText,
+              style: TextStyle(
+                fontSize: 14,
+                color: Colors.black,
+              ),
+            ),
+            action,
+          ],
         ),
       ),
     );
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,22 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
-
     return Scaffold(
       body: SafeArea(
-        child: SizedBox(
-          height: MediaQuery.of(context).size.height*0.3,
+        child: Column(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.3,
+            ),
+            Image.asset(
+              '/assets/login_logo.png',
+              width: 200,
+              height: 200,
+            ),
+          ],
         ),
-
       ),
-
     );
   }
 }
