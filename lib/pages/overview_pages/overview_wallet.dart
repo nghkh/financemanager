2c414018b06cb6546953c2a263a6dcdedab2ba23
nghkh <khoa.bach01@gmail.com>
2022-09-19 14:09:40 +0700

    update wallet page

--- lib/pages/overview_pages/overview_wallet.dart
+++ lib/pages/overview_pages/overview_wallet.dart
@@ -1,9 +1,23 @@
+import 'package:baitap/constant/text_style.dart';
 import 'package:flutter/material.dart';
+
 class OverviewWallet extends StatelessWidget {
   const OverviewWallet({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Column(
+          children: [
+            Text('Số dư',
+              style: AppTextStyle.textStyle3Grey,),
+            Text('100000 VNĐ',
+              style: AppTextStyle.textStyle1,),
+          ],
+        ),
+        bottom:,
+      ),
+    );
   }
 }
