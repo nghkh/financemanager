1f040a65269b8e75d28a2fe551d2981d4538efde
nghkh <khoa.bach01@gmail.com>
2022-09-21 14:46:59 +0700

    update firebase

---
+++ lib/firebase/controller.dart
@@ -0,0 +1,68 @@
+import 'package:baitap/generated_plugin_registrant.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
+
+class AuthController extends GetxController {
+  static AuthController authInstance = Get.find();
+  late Rx<User?> firebaseUser;
+
+  @override
+  void onReady() {
+    super.onReady();
+    firebaseUser = Rx<User?>(auth.currentUser);
+    firebaseUser.bindStream(auth.userChanges());
+
+    ever(firebaseUser, _setInitialScreen);
+  }
+
+  _setInitialScreen(User? user) {
+    if (user != null) {
+      // user is logged in
+      Get.offAll(() => const Home());
+    } else {
+      // user is null as in user is not available or not logged in
+      Get.offAll(() => Login());
+    }
+  }
+
+  void register(String email, String password) async {
+    try {
+      await auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      // this is solely for the Firebase Auth Exception
+      // for example : password did not match
+      print(e.message);
+      // Get.snackbar("Error", e.message!);
+      Get.snackbar(
+        "Error",
+        e.message!,
+        snackPosition: SnackPosition.BOTTOM,
+      );
+    } catch (e) {
+      // this is temporary. you can handle different kinds of activities
+      //such as dialogue to indicate what's wrong
+      print(e.toString());
+    }
+  }
+
+  void login(String email, String password) async {
+    try {
+      await auth.signInWithEmailAndPassword(email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      // this is solely for the Firebase Auth Exception
+      // for example : password did not match
+      print(e.message);
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  void signOut() {
+    try {
+      auth.signOut();
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+}
