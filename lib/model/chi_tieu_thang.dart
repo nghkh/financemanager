d11ae470e61fe5c0edd5b5fdbe105da14f64adc4
zentym <haducoo001@gmail.com>
2022-10-06 17:44:02 +0700

    update

--- lib/model/chi_tieu_thang.dart
+++ lib/model/chi_tieu_thang.dart
@@ -1,32 +1,28 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChiTieuThang {
-  ChiTieuThang({
+  ChiTieuThang(
+    this.iduser,
+    this.tongchiphi, {
     required this.id,
-    required this.iduser,
     required this.thang,
-    required this.tongchiphi,
   });
   final String id;
   final String iduser;
   final String thang;
   final String tongchiphi;
 
-  factory ChiTieuThang.fromSnapshot(
-      DocumentSnapshot<Map<String, dynamic>> snapshot) {
-    return ChiTieuThang(
-      id: snapshot.id,
-      iduser: snapshot.data()!['iduser'],
-      thang: snapshot.data()!['thang'],
-      tongchiphi: snapshot.data()!['tongchiphi'],
-    );
-  }
+  ChiTieuThang.fromSnapshot(DocumentSnapshot snapshot)
+      : id = snapshot.id,
+        iduser = snapshot['iduser'],
+        thang = snapshot['thang'],
+        tongchiphi = snapshot['tongchiphi'];
   factory ChiTieuThang.fromJson(Map<String, dynamic> json) {
     return ChiTieuThang(
+      json['iduser'],
+      json['tongchiphi'],
       id: json['id'],
-      iduser: json['iduser'],
       thang: json['thang'],
-      tongchiphi: json['tongchiphi'],
     );
   }
   Map<String, dynamic> toJson() {
