72ecda9528a4f5a2250f4f109d5d810a58c2efe8
nghkh <khoa.bach01@gmail.com>
2022-10-12 18:41:36 +0700

    update by nghkh

--- lib/pages/overview_pages/controller/overview_account_controller.dart
+++ lib/pages/overview_pages/controller/overview_account_controller.dart
@@ -2,6 +2,19 @@ import 'package:baitap/pages/login_signup/controller/controller_login.dart';
 import 'package:get/get.dart';
 
 class AccountController extends GetxController{
+var dataFirstName = ''.obs;
+var dataLastName = ''.obs;
+var dataGender = ''.obs;
+var dataDoB = ''.obs;
+var phoneNumber = ''.obs;
+var dataEmail = ''.obs;
+
+@override
+  void onInit() {
+
+    // TODO: implement onInit
+    super.onInit();
+  }
 
 }
 
